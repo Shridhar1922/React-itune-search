0b0482b18232d40b6e094a1c3c871e5ddc19eea8
Shridhar Bandgar <shridharbandgar982@gmail.com>
2020-06-04 10:00:49 +0530

    itune search

---
+++ app/services/repoApi.js
@@ -0,0 +1,7 @@
+import { generateApiClient } from '@utils/apiUtils';
+const repoApi = generateApiClient('github');
+
+export const getRepos = repoName =>
+  //repoApi.get(`/search/repositories?q=${repoName}`);
+  repoApi.get(`/search?term=${repoName}`);
+
